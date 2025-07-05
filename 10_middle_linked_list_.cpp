94db6965afc4cfcc2572bfe725d3d199e284cf64
Saurabh Kumar <124427550+kumar02saurabh@users.noreply.github.com>
2025-07-05 23:51:14 +0530

    Create 10_middle_linked_list_.cpp

---
+++ 10_middle_linked_list_.cpp
@@ -0,0 +1,22 @@
+class Solution 
+{
+public:
+    ListNode* middleNode(ListNode* head) {
+        int count = 0;
+        ListNode*temp = head;
+        while(temp != NULL)
+        {
+            count++;
+            temp = temp ->next;
+        }
+        temp = head;
+        count = count / 2;
+        while(count--)
+        {
+            temp = temp->next;
+        }
+        return temp;// return address
+        //return temp->data; // return data 
+        
+    }
+};
